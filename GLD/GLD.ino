8ce20a6a30bc0da0a5b031cefdd2235b82f0d47d
Om Dhumal <94060153+om-dhumal@users.noreply.github.com>
2022-02-17 18:23:23 +0530

    Update GLD.ino

--- GLD/GLD.ino
+++ GLD/GLD.ino
@@ -1,9 +1,29 @@
+#define redLed 9
+#define greenLed 10
+#define buzzervcc 11
+#define buzzertone 12
 void setup() {
-  // put your setup code here, to run once:
-
+  pinMode(redLed,OUTPUT);
+  pinMode(greenLed,OUTPUT);
+  pinMode(buzzervcc,OUTPUT);
+  pinMode(buzzertone,OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  int gasReading = analogRead(A0),min_level = 300;
+  Serial.print(gasReading);
+  Serial.println();
+  delay(500);
+  if(gasReading > min_level){
+    digitalWrite(greenLed,LOW);
+    digitalWrite(redLed,HIGH);
+    digitalWrite(buzzervcc,HIGH);
+    tone(buzzertone,1000);
+  }
+  else{
+    digitalWrite(greenLed,HIGH);
+    digitalWrite(redLed,LOW);
+    digitalWrite(buzzervcc,LOW);
+  }
 }
